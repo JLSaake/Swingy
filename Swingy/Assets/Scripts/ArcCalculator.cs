8f1b8f5380c070a0e3ed753e5ccf0f053381474e
Christopher Chu <cdipalma@uci.edu>
2019-01-26 01:54:09 -0800

    GIT FIRES TOTALLY DESCRIPTIVE NAME

--- Swingy/Assets/Scripts/ArcCalculator.cs
+++ Swingy/Assets/Scripts/ArcCalculator.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ArcCalculator : MonoBehaviour
 {
+
+    static float maxVelocity = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,18 @@ public class ArcCalculator : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public static Vector2 posAtTime(float time, float angle)
+    {
+        return new Vector2(0.0f, 0.0f);
+        // Angle is a degree value between 0 and 90. (0 is right, 90 is up)
+    }
+
+    public static Vector2 maxPosAtTime(float time)
+    {
+        float ScaledVelocity = maxVelocity * 0.7071f;
+        float vx = maxVelocity / 2.0f, vy = vx;
+        return new Vector2(vx * time, -0.681f * Mathf.Pow(time, 2) + vy * time);
     }
 }
--- Swingy/Assets/Scripts/Procedural.cs
+++ Swingy/Assets/Scripts/Procedural.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+public enum HeightIncrement
+{
+    Linear,
+    Decrease
+};
+
 public class Procedural : MonoBehaviour
 {
 
     // gIVE mE rOPE
     public GameObject rope;
+    // [Range(0.5f, 2.0f)]
+    public float ropeLengthLow;
+    public float ropeLengthHigh;
+    public float averageRopeSpacing;
+    [Range(0.01f, 0.5f)]
+    public float ropeSpacingVariation;
 
     // Level parameters
     int maxHeight;
+    float lastX, lastY;
+    HeightIncrement heightIncrement;
 
     // Internal state management per level
     int height;
@@ -22,9 +36,10 @@ public class Procedural : MonoBehaviour
     void Start()
     {
         Debug.Log("Start");
-        configureParams(16);
+        configureParams(16, new Vector2(0,0), HeightIncrement.Decrease);
         rand = new System.Random();
-        for(int i=0; i<40; ++i) Instantiate(rope, new Vector2((float)i, (float)nextRope()), Quaternion.identity);
+        for(int i=0; i<40; ++i) Instantiate(rope, nextRope(), Quaternion.identity);
+        // for(int t=0; t<50; ++t) Instantiate(rope, ArcCalculator.maxPosAtTime(t/5), Quaternion.identity);
     }
 
     // Update is called once per frame
@@ -33,32 +48,48 @@ public class Procedural : MonoBehaviour
         
     }
 
-    void configureParams(int maxHeight)
+    void configureParams(int maxHeight, Vector2 startPos, HeightIncrement heightIncrement)
     {
         this.maxHeight = maxHeight;
+        this.heightIncrement = heightIncrement;
+        this.lastX = startPos.x;
+        this.lastY = startPos.y;
 
         // Reset internal level state
         height = 0;
         climbChance = 0.30f;
     }
 
-    int nextRope()
+    Vector2 nextRope()
     {
+        float xPos = lastX + averageRopeSpacing * ((float)(rand.NextDouble() + rand.NextDouble())*ropeSpacingVariation + 1f - ropeSpacingVariation);
+        lastX = xPos;
         if(height < maxHeight)
         {
             bool climb = rand.NextDouble() < climbChance;
             if(climb)
             {
                 ++height;
-                climbChance = 0.05f + ;
+                climbChance = 0.05f;
             }
             else
             {
-                climbChance += 0.35f * (maxHeight - height) / maxHeight;
+                switch(heightIncrement)
+                {
+                    case HeightIncrement.Linear:
+                        climbChance += 0.30f;
+                        break;
+                    case HeightIncrement.Decrease:
+                        climbChance += 0.45f * (maxHeight - height) / maxHeight;
+                        break;
+                    default:
+                        Debug.Log("");
+                }
+                
             }
         }
         // Debug.Log("hi" + climbChance);
-        return height;
+        return new Vector2(xPos, (float)height);
     }
 
 }
