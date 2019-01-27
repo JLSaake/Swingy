9c230b89e567995afa775d5a74f670abbf32186c
Jake <jesmith24@comcast.net>
2019-01-26 22:58:40 -0800

    Color select transition finished

--- Swingy/Assets/Scripts/ColorSelect.cs
+++ Swingy/Assets/Scripts/ColorSelect.cs
@@ -1,33 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ColorSelect : MonoBehaviour
 {
     const int NUM_COLORS = 8;
+
+    public float shrinkRate = 1.0f;
+
     [SerializeField]
     private List<GameObject> panels;
-
-    // Selling cache coherency 10mil GE
-    private List<Material> flowMats = new List<Material>();
-    private List<Animator> flowAnims = new List<Animator>();
-
-    // Forgive my dear aunt sally for parallel arrays
-    // The idea is bearable because the panel array is set once and only once
-
     [SerializeField]
     private List<Color> panelColors;
+
+    [SerializeField]
+    private List<TextMeshPro> labelTMP = new List<TextMeshPro>();
 
     // TODO: UI element to interpolate each panel to these
     [SerializeField]
     private List<Color> colorBlindPanelColors;
+
+    // Selling cache coherency 10mil GE
+    private List<Material> flowMats = new List<Material>();
+    private List<Animator> flowAnims = new List<Animator>();
 
     // State management
     // TODO: debug mode that selects colors and gets into the game
     private bool cb; // colorblind
     private int selectIndex = 0; 
     private int lastDirection = 0;
+    private int promptStage = 0;
     private bool[] selectedPanels = new bool[8];
+
+    private List<string> prompts = new List<string>()
+    {
+        "safety?", "familiar?", "family?", "lol"
+    };
 
     void Start()
     {
@@ -71,10 +80,12 @@ public class ColorSelect : MonoBehaviour
             cycleSelection((int)(Input.GetAxisRaw("Horizontal")));
         }
 
-        if (Input.GetButtonDown("Jump")) 
+        if (Input.GetButtonDown("Jump") && promptStage < 3) 
         {
             // Play the animation for selecting the color
             flowAnims[selectIndex].Play("Pick");
+
+
             // Play a cool sound effect for selecting
 
             // Prevent picker from cycling into this color again
@@ -84,10 +95,17 @@ public class ColorSelect : MonoBehaviour
             if (GameManager.AddColor(cb ? colorBlindPanelColors[selectIndex] : panelColors[selectIndex]) == 3)
             {
                 // fade this out, move on
-                Debug.Log("Nope!");
+                StartCoroutine(lerpPrompt(0, false));
+                StartCoroutine(lerpPrompt(1, false));
+                StartCoroutine(shrink()); 
             }
             else
             {
+                // Lerp the prompt to the next prompt
+                promptStage++;
+                StartCoroutine(lerpPrompt(1, true));
+
+                // Prep for next input
                 lastDirection = 0;
                 cycleSelection(1);
             }
@@ -133,5 +151,41 @@ public class ColorSelect : MonoBehaviour
             flowMats[index].SetFloat("_FlowActive", alpha);
             yield return null;
         } while (alpha < end);
+    }
+
+    // Label = 1 or 2
+    // twice = 1 -> 0 AND 0 -> 1
+    private IEnumerator lerpPrompt(int label, bool twice)
+    {
+        float alpha = 1.0f;
+        TextMeshPro tmp = labelTMP[label];
+        do {
+            alpha -= Time.deltaTime * shrinkRate;
+            tmp.color = new Color(tmp.color.r,tmp.color.g,tmp.color.b,alpha);
+            yield return null;
+        } while (alpha >= 0.0f);
+
+        if (twice) 
+        {
+            tmp.text = prompts[promptStage];
+            do {
+                alpha += Time.deltaTime * shrinkRate;
+                tmp.color = new Color(tmp.color.r,tmp.color.g,tmp.color.b,alpha);
+                yield return null;
+            } while (alpha <= 1.0f);
+        }
+    }
+
+    private IEnumerator shrink()
+    {
+        float alpha = 0.0f;
+        Vector3 startPos = transform.position;
+        Vector3 endPos = new Vector3(0f,8.3f,0f);
+        do {
+            alpha += Time.deltaTime * shrinkRate * 0.25f;
+            transform.position = Vector3.Lerp(startPos,endPos,alpha);
+            yield return null;
+        } while (alpha <= 1.0f);
+        // TODO: use GameManager to yield control to the player
     }
 }
