be1fcb69d0ef4d7692aecb83bb1626efea5b0ef3
John Luke Saake <johnlukesaake@gmail.com>
2019-01-26 17:27:01 -0800

    More Obstacles
    Added blocker obstacle and animation
    TODO: variables that adjust animation / scale parameters for blocker and spikes

---
+++ Swingy/Assets/Scripts/FlyingEnemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemy : Obstacle
+{
+    public float waveHeight = 1.25f;
+    public float waveSpeed = 0.05f;
+    private Vector2 startLocation;
+    private float negCounter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startLocation = this.gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        negCounter -= waveSpeed;
+
+        this.gameObject.transform.position = new Vector2(startLocation.x + negCounter, startLocation.y + Mathf.Sin(negCounter) * waveHeight);
+    }
+
+
+}
